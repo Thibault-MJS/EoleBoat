b6823f08288f1ed8cb15e6d6608439b6a85303af
Thibault-MJS <68784661+Thibault-MJS@users.noreply.github.com>
2020-07-31 19:03:54 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,6 @@ const { Client } = require('discord.js'); // !important
  * @param {Client} bot 
  */
 module.exports = bot => {
-    var status = [`${bot.guilds.cache.size} serveur(s) | ${bot.config.prefix}help.`, `${bot.users.cache.filter(user => !user.bot)} utilisateur(s) | ${bot.config.prefix}help.`];
-    setInterval(() => bot.user.setActivity(status[0]).then(() => setTimeout(() => bot.user.setActivity(status[1]), 2000)), 4000);
+    var status = [`${bot.guilds.cache.size} serveur(s) | ${bot.config.prefix}help.`, `${bot.users.cache.filter(user => !user.bot).size} utilisateur(s) | ${bot.config.prefix}help.`];
+    setInterval(() => bot.user.setActivity(status[0]).then(() => setTimeout(() => bot.user.setActivity(status[1]), 4000)), 8000);
 }
